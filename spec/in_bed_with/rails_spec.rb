a0e5e8e0600be02135a43f9fb8b42ec303b2246f
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-28 21:08:49 +1030

    Adjust rails spec

--- spec/in_bed_with/rails_spec.rb
+++ spec/in_bed_with/rails_spec.rb
@@ -11,9 +11,10 @@ describe ActionView::Base do
   it 'should add in_bed_with helper through railtie' do
     InBedWith.config { |config| config.analytics_property_id = 'UA-7654321-1' }
 
-    ActionView::Base.should respond_to(:in_bed_with)
+    view = ActionView::Base.new
+    view.should respond_to(:in_bed_with)
 
-    ActionView::Base.in_bed_with.should           == InBedWith
-    ActionView::Base.in_bed_with.analytics.should =~ /UA-7654321-1/
+    view.in_bed_with.should           == InBedWith
+    view.in_bed_with.analytics.should =~ /UA-7654321-1/
   end
 end
