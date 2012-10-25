e59453cb434eeacb24649551d681cd25f2910862
Steven Fuchs <stwf@yahoo.com>
2012-10-24 22:54:30 -0400

    new stuff

--- lib/in_bed_with/quantcast.rb
+++ lib/in_bed_with/quantcast.rb
@@ -4,29 +4,28 @@ module InBedWith
 
     	def code
 			[
-				%(<script type="text/javascript">),
-				%(var _qevents = _qevents || [];),
+				'<script type="text/javascript">',
+				'var _qevents = _qevents || [];',
 
-				%((function() {),
-				%(var elem = document.createElement('script');),
-				%(elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";),
-				%(elem.async = true;),
-				%(elem.type = "text/javascript";),
-				%(var scpt = document.getElementsByTagName('script')[0];),
-				%(scpt.parentNode.insertBefore(elem, scpt);),
-				%(})();),
+				'(function() {',
+				'var elem = document.createElement("script");',
+				'elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";',
+				'elem.async = true;',
+				'elem.type = "text/javascript";',
+				'var scpt = document.getElementsByTagName("script")[0];',
+				'scpt.parentNode.insertBefore(elem, scpt);',
+				'})();',
 
-				%(_qevents.push({),
-				%(qacct:"#{account_name}"),
-				%(});),
-				%(</script>),
+				'_qevents.push({',
+				'qacct:"#{account_name}"',
+				'});',
+				'</script>',
 
-				%(<noscript>),
-				%(<div style="display:none;">),
-				%(<img src="//pixel.quantserve.com/pixel/p-c0-15CjNXRBjM.gif" border="0" height="1" width="1" alt="Quantcast"/>),
-				%(</div>),
-				%(</noscript>)
-			].join("")
+				'<noscript>',
+				'<div style="display:none;">',
+				'<img src="//pixel.quantserve.com/pixel/p-c0-15CjNXRBjM.gif" border="0" height="1" width="1" alt="Quantcast"/>',
+				'</div>',
+				'</noscript>'].join("")
 		end
 	end
 end
